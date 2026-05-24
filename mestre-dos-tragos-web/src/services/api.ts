e9bc119c16fabd0aebdba7f4423314a05bae3cb6
Matheus Silva Ribeiro <matheusfloripa07@gmail.com>
2026-05-24 20:22:49 -0300

    fix: corrige url da api em producao

--- mestre-dos-tragos-web/src/services/api.ts
+++ mestre-dos-tragos-web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://mestre-dos-tragos-api.onrender.com',
+  baseURL: 'https://mestre-dos-tragos-api.onrender.com/api',
 });
 
 // 
